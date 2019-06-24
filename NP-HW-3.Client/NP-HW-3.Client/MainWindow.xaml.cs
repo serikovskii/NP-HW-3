045ad57dc7ce100842fd3684f30ad5034782dfe7
Azat <46015824+serikovskii@users.noreply.github.com>
2019-06-25 01:10:21 +0600

    добавить возможность отправлять индивидуально сообщ

---
+++ NP-HW-3.Client/NP-HW-3.Client/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NP_HW_3.Client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TcpClient client;
+        private Thread listenThread;
+        private Thread sendThread;
+        private bool buttonIsEnabled;
+        private string messageName;
+        private List<string> namesUser;
+        public MainWindow()
+        {
+            InitializeComponent();
+            buttonIsEnabled = false;
+            namesUser = new List<string>();
+        }
+
+        private void ConnectedButton(object sender, RoutedEventArgs e)
+        {
+            if (!buttonIsEnabled)
+            {
+                try
+                {
+                    client = new TcpClient();
+                    client.Connect(IPAddress.Parse(ip.Text), int.Parse(port.Text));
+                    listenThread = new Thread(LisetenThreadProc);
+                    listenThread.Start(client);
+                    sendThread = new Thread(SendThreadProc);
+                    sendThread.Start(client);
+                    buttonIsEnabled = true;
+                    connect.Content = "Stop";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                sendThread.Abort();
+                listenThread.Abort();
+                client.Close();
+
+                buttonIsEnabled = false;
+                connect.Content = "Start";
+            }
+        }
+        public void LisetenThreadProc(object obj)
+        {
+            var client = obj as TcpClient;
+            byte[] buffer;
+
+            while (true)
+            {
+                buffer = new byte[1024 * 4];
+                var reciveSize = client.Client.Receive(buffer);
+                if(Encoding.UTF8.GetString(buffer, 0, reciveSize).Contains("name"))
+                {
+                    //var byteBuf = Encoding.UTF8.GetString(buffer, 4, reciveSize);
+                    namesUser.Add(Encoding.UTF8.GetString(buffer, 4, reciveSize));
+                    var list = Encoding.UTF8.GetString(buffer, 4, reciveSize);
+                }
+                else 
+                    Dispatcher.Invoke(() => chat.AppendText($"{Encoding.UTF8.GetString(buffer, 0, reciveSize)} \n"));
+                Dispatcher.Invoke(() => clients.ItemsSource = namesUser);
+                buffer = null;
+            }
+        }
+
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                sendThread = new Thread(SendThreadProc);
+                sendThread.Start(client);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SendThreadProc(object obj)
+        {
+            var client = obj as TcpClient;
+            string messageToServer = "";
+            if (messageName == null)
+            {
+                Dispatcher.Invoke(() => messageName = name.Text);
+                client.Client.Send(Encoding.GetEncoding(1251).GetBytes(messageName));
+            }
+            else
+            {
+                Dispatcher.Invoke(() => messageToServer = message.Text);
+                client.Client.Send(Encoding.GetEncoding(1251).GetBytes(messageToServer));
+
+            }
+
+        }
+    }
+}
---
+++ NP-HW-3.Server/NP-HW-3/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NP_HW_3
+{
+    class Program
+    {
+        static TcpListener server;
+        static Thread thread;
+        static ManualResetEvent eventStop;
+        static Dictionary<string, TcpClient> clients;
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                eventStop = new ManualResetEvent(false);
+                server = new TcpListener(IPAddress.Any, 12345);
+                server.Start();
+                Console.WriteLine("Ожидани подключения");
+                thread = new Thread(ServerThreadProcedure);
+                thread.Start(server);
+                clients = new Dictionary<string, TcpClient>();
+            }
+            catch (Exception exctption)
+            {
+                Console.WriteLine(exctption.Message, "Error");
+            }
+        }
+        static void ServerThreadProcedure(object obj)
+        {
+            TcpListener server = (TcpListener)obj;
+
+            while (true)
+            {
+                IAsyncResult asyncResult = server.BeginAcceptSocket(AsyncServerProc, server);
+                while (asyncResult.AsyncWaitHandle.WaitOne(200) == false)
+                {
+                    if (eventStop.WaitOne(0) == true)
+                        return;
+                }
+
+            }
+        }
+
+        static void AsyncServerProc(IAsyncResult iAsync)
+        {
+
+            TcpListener server = (TcpListener)iAsync.AsyncState;
+            TcpClient client = server.EndAcceptTcpClient(iAsync);
+            Console.WriteLine("Подключился клиент");
+            Console.WriteLine("IP адрес клиента " + client.Client.RemoteEndPoint.ToString() + "\n");
+            ThreadPool.QueueUserWorkItem(ClientThreadProc, client);
+
+        }
+
+        static void ClientThreadProc(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+
+            Console.WriteLine("Рабочий поток клиента запущен");
+            var buffer = new byte[1024 * 4];
+
+            string clientName;
+            string messageClient = "";
+            string messageServer = "";
+            int reciveSize;
+            int reciveSizeName;
+
+            reciveSizeName = client.Client.Receive(buffer);
+            clientName = Encoding.GetEncoding(1251).GetString(buffer, 0, reciveSizeName);
+            clients.Add(clientName, client);
+            Console.WriteLine($"Клиент {clientName} \r\n");
+
+            for (int i = 0; i < clients.Count(); i++)
+            {
+                clients.ElementAt(i).Value.Client.Send(Encoding.GetEncoding(1251).GetBytes($"New user connected {clientName}"));
+            }
+
+
+
+
+            while (true)
+            {
+
+                var ipClient = client.Client.RemoteEndPoint.ToString();
+                reciveSize = client.Client.Receive(buffer);
+                if (reciveSize == 0)
+                {
+                    Console.WriteLine("stop");
+                    clients.Clear();
+                    break;
+                }
+                messageClient = Encoding.GetEncoding(1251).GetString(buffer, 0, reciveSize);
+                messageServer = $"{clientName}: {messageClient} \n";
+
+                for (int i = 0; i < clients.Count(); i++)
+                {
+                    clients.ElementAt(i).Value.Client.Send(Encoding.GetEncoding(1251).GetBytes(messageServer));
+                }
+
+
+
+            }
+        }
+    }
+}
